80d06e95674ec6628db623a313889c0929bd54a8
ihfazhillah <mihfazhillah@gmail.com>
2019-05-06 07:21:28 +0700

    [master] add simple main file

---
+++ main.py
@@ -0,0 +1,40 @@
+import pprint
+import click
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = 'https://stackoverflow.com'
+SEARCH = f"{BASE_URL}/search?q="
+
+
+def search(query):
+    """Search query through stackoverflow search page
+
+    :query: TODO
+    :returns: TODO
+
+    """
+    resp = requests.get(f"{SEARCH}{query}")
+    soup = BeautifulSoup(resp.content)
+
+    result_count = soup.select_one(".fl1.fs-body3").text
+    search_results = soup.select(".search-result .result-link a")
+    next_ = soup.select_one("[rel='next']")['href']
+
+    results = [{
+        "title": result['title'],
+        "url": f"{BASE_URL}{result['href']}"
+    } for result in search_results]
+
+    return {'count': result_count, 'results': results, 'next': f"{BASE_URL}{next_}"}
+
+
+@click.command()
+@click.argument('query')
+def searching(query):
+    result = search(query)
+    pprint.pprint(result, indent=4)
+
+
+if __name__ == "__main__":
+    searching()
